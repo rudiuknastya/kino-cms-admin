4721ac61beab7ce71ca7dee7bb92abfdfac48f70
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-09-13 12:55:03 +0300

    Added static configuration

--- src/main/java/project/config/MvcConfig.java
+++ src/main/java/project/config/MvcConfig.java
@@ -14,5 +14,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file://"+uploadPath+"/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
